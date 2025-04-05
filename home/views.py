bfe22e304cff232e3ee509b6f59d78ef757e0192
George Sterpu <george.sterpu@gmail.com>
2025-04-05 21:58:05 +0100

    update

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Coming soon.")
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "index.html", {"year": datetime.datetime.now().year})
+
+def notify(request):
+    email = request.POST.get("email")
+    # Save email to DB or mailing list here
+    return HttpResponse('<div class="text-success fw-semibold">Thanks! We’ll be in touch soon. 🚀</div>')
